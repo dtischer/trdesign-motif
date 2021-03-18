6664a3a1a742c3c833bc73b767e18840a5cba5e0
Jue Wang <juewang@post.harvard.edu>
2021-03-17 18:02:27 -0700

    add ss frac command

--- scoring/calc_metrics.sh
+++ scoring/calc_metrics.sh
@@ -23,6 +23,7 @@ pre=`basename $1`
 sbatch -J lddt.$pre $DIR/predict_lddt.sh $1
 $DIR/calc_cce.sh $1 cce.$pre
 sbatch -J frag.$pre $DIR/pick_frags_calc_qual.sh $1
+sbatch -J ss.$pre -c 2 --mem=6g --wrap="$DIR/get_ss_frac.py $1"
 if [ "$#" -eq 3 ]; then
     sbatch -J pymetric.$pre --mem=8g --wrap="$DIR/pymol_metrics.py --template $2 --receptor $3 $1"
 elif [ "$#" -eq 4 ]; then
