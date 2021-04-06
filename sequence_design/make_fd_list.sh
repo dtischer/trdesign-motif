b3c2b30617029365e80cb2ce866d630d9d984b71
Jue Wang <juewang@post.harvard.edu>
2021-04-06 13:30:04 -0700

    trim_tails.py now breaks disulfides before trimming removed some .ipynb_checkpoint folders added -s option to calc_metrics.sh

--- sequence_design/make_fd_list.sh
+++ sequence_design/make_fd_list.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #
 # Generates FastDesign commands for a folder of hallucinations and outputs to
-# stdout. Run them using submit_fd.sh
+# stdout. Run them using submit_fd.sh. This does not generate commands for
+# designs that already have outputs (in FOLDER/fast_designs/chA) so delete them
+# if you're trying to repeat runs.
 #
 # Usage (redirecting output to task list file): 
 #
-#   ./make_fd_list.sh FOLDER TEMPLATE_PDB RECEPTOR_PDB INTERFACE_RES_FILE > fd.list
+#   ./make_fd_list.sh FOLDER TEMPLATE_PDB RECEPTOR_PDB INTERFACE_RES_FILE >
+#   fd.list
 #
 # FOLDER should contain PDBs of structures you want to design, along with TRB
 # files mapping their constrained regions to the template.
@@ -29,14 +32,16 @@ for PDB in $1/*.pdb; do
     logfile=log/$ID.fd.log
 
     cmd="cd $ABS1; "
-    if [ ! -f trimmed/$ID.pdb ]; then
+    if [ ! -f $1/trimmed/$ID.pdb ]; then
         cmd+="$DIR/trim_tails.py --pdb `basename $PDB` --suffix='' --out_dir trimmed/ &>> $logfile; "
     fi
     TRIMMED=trimmed/`basename $PDB`
-    if [ ! -f pssm/${ID}_0001.profile.pssm ]; then
+    if [ ! -f $1/pssm/${ID}_0001.profile.pssm ]; then
         cmd+="$DIR/pssm/mk_pssm.sh $TRIMMED &>> $logfile; "
     fi
-    cmd+="$DIR/structure2design.py --pdb_in $TRIMMED --trb_file $TRB --out_dir ./ --native $ABS2 --tar $ABS3 --freeze_native_residues `cat $ABS4` --pssm_file=pssm/${ID}_0001.profile.pssm --pssm_mode=norn1 --layer_design=True &>> $logfile"
-    echo $cmd
+    if [ ! -f $1/fast_designs/chA/$ID.pdb ]; then
+        cmd+="$DIR/structure2design.py --pdb_in $TRIMMED --trb_file $TRB --out_dir ./ --native $ABS2 --tar $ABS3 --freeze_native_residues `cat $ABS4` --pssm_file=pssm/${ID}_0001.profile.pssm --pssm_mode=norn1 --layer_design=True &>> $logfile"
+        echo $cmd
+    fi
 done  
 
